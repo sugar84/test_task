7ea3d01deb1d4866ebc62b22fec1e6ec658ac487
antonuk84@yandex.ru <sugar@singularity.localdomain>
2010-12-08 00:37:01 +0300

    some changes, start to duild in db

--- lib/test_task.pm
+++ lib/test_task.pm
@@ -1,10 +1,24 @@
 package test_task;
 use Dancer ':syntax';
+use Dancer::Plugin::Database;
+use Carp;
 
 our $VERSION = '0.1';
 
 get '/' => sub {
     template 'index';
+};
+
+get '/page_1' => sub {
+    my $sth = database->prepare(
+        'select * from books',
+    );
+    $sth->execute
+        or croak $sth->errstr;
+
+    template "base", { 
+        records => database->fetchall_arrayref,
+    };
 };
 
 true;
