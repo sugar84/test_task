3c7a07b35a406a6673a0727568f94c6e1b99e348
Anton Ukolov <antonuk84@yandex.ru>
2010-12-09 14:01:39 +0300

    added states, comment_on var, changed views

--- lib/test_task.pm
+++ lib/test_task.pm
@@ -5,18 +5,21 @@ use Carp;
 
 our $VERSION = '0.1';
 
+my $comment_on;
+
 get '/' => sub {
     template 'index';
 #    render_with_layout 'index';
 };
 
-get '/page_1' => sub {
+get '/page/:id' => sub {
     my $sth = database->prepare(
         'select * from books',
     );
     $sth->execute
         or croak $sth->errstr;
 
+    $comment_on = params->{id};
     template "base", { 
         records => $sth->fetchall_hashref("id"),
         test    => "hello there!",
@@ -30,10 +33,13 @@ get '/error' => sub {
 };
 
 get '/comment' => sub {
+    my $to_page = $comment_on;
+    undef $comment_on;
+    
     template 'comment', {
         comment_url => uri_for("/comment"),
+        comment_on  => $to_page,
     };
-
 };
 
 #before_template sub {
